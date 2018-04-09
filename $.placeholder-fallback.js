9dfcf326a6db2ac5ca2a1ecbad6eb155c0a3ec57
aurelienlhx <dev@aurelienlheureux.com>
2018-04-09 15:08:16 +0200

    Create $.placeholder-fallback.js

---
+++ $.placeholder-fallback.js
@@ -0,0 +1,16 @@
+(function($){
+	$(document).ready(function(){
+		
+		if(!('placeholder' in  document.createElement('input')))
+		{
+			$("input[type=text],textarea").each(function(i){
+				var placeholder = $(this).attr("placeholder") || false;
+				if(!placeholder) return;
+
+				if(!$(this).val()) $(this).val(placeholder);
+				$(this).focus(function(){if($(this).val()==placeholder || $(this).val()=="")$(this).val("")})
+				$(this).blur(function(){if($(this).val()==placeholder || $(this).val()=="")$(this).val(placeholder)})
+			});
+		}
+	});
+})(jQuery);
