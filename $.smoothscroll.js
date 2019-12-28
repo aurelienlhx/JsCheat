f574a6786952ad98c088933895d89ea814d7f2bc
aurelienlhx <dev@aurelienlheureux.com>
2019-12-28 17:59:05 +0100

    Update $.smoothscroll.js

--- $.smoothscroll.js
+++ $.smoothscroll.js
@@ -3,12 +3,20 @@
 		$('a[href^="#"]').on('click',function(e){
 			e.preventDefault();
 			var anchor=this.href.substring(this.href.indexOf('#'),this.href.length);
-			var $target = $(anchor);
-			if($target.length>0){
-				$('html,body').animate({
-					scrollTop:$target.offset().top-100
-				},1000)
-			}
+			scrollTo(anchor);
 		})
+        if(document.location.hash && document.location.hash.indexOf("utm") == -1){
+            scrollTo(document.location.hash);
+        }
+            
 	})
-})(jQuery,document)
+
+    function scrollTo(anchor){
+        var $target = $(anchor);
+        if($target.length>0){
+             $('html,body').animate({
+                scrollTop:$target.offset().top-100
+             },1000)
+         }
+    }
+})(jQuery,document);
