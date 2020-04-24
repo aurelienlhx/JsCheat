a51da9c1f027ed551e286102499487404af0dd46
aurelienlhx <dev@aurelienlheureux.com>
2020-04-24 11:26:27 +0200

    Update $.smoothscroll.js

--- $.smoothscroll.js
+++ $.smoothscroll.js
@@ -1,22 +1,25 @@
-(function($,document){
-	$(document).ready(function(){
-		$('a[href^="#"]').on('click',function(e){
-			e.preventDefault();
-			var anchor=this.href.substring(this.href.indexOf('#'),this.href.length);
-			scrollTo(anchor);
-		})
-        if(document.location.hash && document.location.hash.indexOf("utm") == -1){
-            scrollTo(document.location.hash);
-        }
-            
-	})
-
-    function scrollTo(anchor){
-        var $target = $(anchor);
-        if($target.length>0){
-             $('html,body').animate({
-                scrollTop:$target.offset().top-100
-             },1000)
-         }
+(function($, document) {
+  $(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      scrollTo(this.href);
+    })
+    if (document.location.hash && document.location.hash.indexOf("utm") == -1) {
+      scrollTo(document.location.hash);
     }
-})(jQuery,document);
+
+  })
+
+  function scrollTo(anchor) {
+    var found = anchor.match(/(\#\w+)([\+-]{1}\d+)/);
+    if (found && found[1])
+      var $target = $(found[1]);
+    if (found && found[2])
+      var offset = parseInt(found[2]);
+    if (undefined !== $target && $target.length > 0) {
+      $('html,body').animate({
+        scrollTop: $target.offset().top + (offset || 0)
+      }, 1000)
+    }
+  }
+})(jQuery, document);
