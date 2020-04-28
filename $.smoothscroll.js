68be93843b576a90d1959e01aaa588b0dbcda3c7
aurelienlhx <dev@aurelienlheureux.com>
2020-04-28 11:57:40 +0200

    Update $.smoothscroll.js

--- $.smoothscroll.js
+++ $.smoothscroll.js
@@ -1,25 +1,33 @@
-(function($, document) {
+(function($) {
   $(document).ready(function() {
     $('a[href^="#"]').on('click', function(e) {
-      e.preventDefault();
-      scrollTo(this.href);
-    })
+      scrollTo(this.href, e);
+    });
     if (document.location.hash && document.location.hash.indexOf("utm") == -1) {
       scrollTo(document.location.hash);
     }
 
-  })
+  });
 
-  function scrollTo(anchor) {
-    var found = anchor.match(/(\#\w+)([\+-]{1}\d+)/);
+  $(window).on('hashchange', function(e) {
+    scrollTo(document.location.hash, e);
+  });
+
+  function scrollTo(anchor, e) {
+    var found = anchor.match(/(\#\w+)([\+-]{1}\d+)?/);
     if (found && found[1])
       var $target = $(found[1]);
     if (found && found[2])
       var offset = parseInt(found[2]);
+
+
     if (undefined !== $target && $target.length > 0) {
+      if (e) {
+        e.preventDefault();
+      }
       $('html,body').animate({
         scrollTop: $target.offset().top + (offset || 0)
-      }, 1000)
+      }, 500)
     }
   }
-})(jQuery, document);
+})(jQuery);
