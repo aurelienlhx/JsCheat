e1df3d5602746125d0df543101be78dd21822cd9
aurelienlhx <dev@aurelienlheureux.com>
2020-04-28 12:36:29 +0200

    Update $.smoothscroll.js

--- $.smoothscroll.js
+++ $.smoothscroll.js
@@ -15,13 +15,13 @@
 
   function scrollTo(anchor, e) {
     var found = anchor.match(/(\#\w+)([\+-]{1}\d+)?/);
-    if (found && found[1])
+    if (found && found[1] && found[1] !== '#')
       var $target = $(found[1]);
     if (found && found[2])
       var offset = parseInt(found[2]);
 
 
-    if (undefined !== $target && $target.length > 0) {
+    if (undefined !== $target && $target.length > 0 && $target.is(':visible')) {
       if (e) {
         e.preventDefault();
       }
